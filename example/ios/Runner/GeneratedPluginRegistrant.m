e15522d938a2928f1143c5eca419a5aeefc97fd1
danielR2001 <daniel.rachlin@gmail.com>
2021-05-29 01:16:05 +0300

    .

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -4,23 +4,9 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<flutter_exoplayer/AudioPlayerPlugin.h>)
-#import <flutter_exoplayer/AudioPlayerPlugin.h>
-#else
-@import flutter_exoplayer;
-#endif
-
-#if __has_include(<path_provider/FLTPathProviderPlugin.h>)
-#import <path_provider/FLTPathProviderPlugin.h>
-#else
-@import path_provider;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [AudioPlayerPlugin registerWithRegistrar:[registry registrarForPlugin:@"AudioPlayerPlugin"]];
-  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
 }
 
 @end
